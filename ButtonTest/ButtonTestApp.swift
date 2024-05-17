fdf116605cab6163a5acd599168851a990bea29d
Hunter Treadway <treadwayhunter@gmail.com>
2024-05-17 12:51:57 -0500

    Initial Commit

---
+++ ButtonTest/ButtonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTestApp.swift
+//  ButtonTest
+//
+//  Created by Hunter Treadway on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ButtonTest
+//
+//  Created by Hunter Treadway on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
