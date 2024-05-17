c43a3c462b300cf59039d6b3d875d7e1fec480d6
Hunter Treadway <treadwayhunter@gmail.com>
2024-05-17 16:32:05 -0500

    Init commit. Got buttons looking nice.

---
+++ ButtonTest/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  ButtonTest
+//
+//  Created by Hunter Treadway on 5/17/24.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
